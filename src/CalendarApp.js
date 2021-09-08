5da8bf9d0b7bb7dec31ed0eec4863e7c8cdf2cf8
Valentina Gomez <valentinagomezgiraldo11@gmail.com>
2021-09-08 15:47:11 -0500

    Redux configuration

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -1,10 +1,13 @@
-import React from 'react'
-import {AppRouter} from './router/AppRouter'
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store/store";
+
+import { AppRouter } from "./router/AppRouter";
 
 export const CalendarApp = () => {
-    return (
-        <>
-            <AppRouter/>
-        </>
-    )
-}
+  return (
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>
+  );
+};
